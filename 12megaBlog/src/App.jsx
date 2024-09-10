bb00b5a5bf4091c45459e672e4ea83af5343612b
Vihan Patidar <1vihanpatidar6@gmail.com>
2024-09-10 21:51:54 +0530

    red toolkit

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -1,13 +1,28 @@
-
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "../appwrite/auth";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>vihan patidar</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400"></div>
+  ) : null;
 }
 
-export default App
+export default App;
